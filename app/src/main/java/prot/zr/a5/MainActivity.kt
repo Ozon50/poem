70d8d5ea4603e7b56f06ef6372a802916a1ef57b
Ozon50 <ozon50505050@mail.ru>
2021-12-08 11:29:11 +0700

    DZ3

--- app/src/main/java/prot/zr/a5/MainActivity.kt
+++ app/src/main/java/prot/zr/a5/MainActivity.kt
@@ -2,13 +2,14 @@ package prot.zr.a5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 const val POEM = "MainActivity"
-var ticker = 0
+public var ticker = 0
 val list_poem = listOf(
     "\tБуря мглою небо кроет,\n",
     "\tВихри снежные крутя;\n",
@@ -18,12 +19,47 @@ val list_poem = listOf(
     "\tВдруг соломой зашумит,\n"
 )
 
+var flag_butt_connect = 0
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewMain: TextView = findViewById(R.id.textViewId)
+        val connectButton: Button = findViewById(R.id.conect_button_id)
+        val readButton: Button = findViewById(R.id.main_read_button)
+        val editText: EditText = findViewById(R.id.editText)
+
+
+        connectButton.setOnClickListener {
+            if (flag_butt_connect == 0) {
+                connectButton.text = getString(R.string.connect)
+                flag_butt_connect = 1
+
+            } else if (flag_butt_connect == 1) {
+                connectButton.text = getString(R.string.disc)
+                flag_butt_connect = 0
+
+            }
+        }
+
+        editText.setOnFocusChangeListener { v, hasFocus -> editText.text.clear() }
+
+
+        readButton.setOnClickListener {
+            textViewMain.text = "Начало начал!"
+        }
+        fun countMe(view: View) {
+
+            val countString = textViewMain.text.toString()
+            var count: Int = Integer.parseInt(countString)
+            count++
+            textViewMain.text = count.toString();
+        }
+
         if (ticker == 0) {
-            Log.d(POEM, list_poem.get(ticker))
+            Log.d(POEM, list_poem[ticker])
             ticker++
         }
     }
@@ -31,7 +67,7 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         if (ticker == 1) {
-            Log.d(POEM, list_poem.get(ticker))
+            Log.d(POEM, list_poem[ticker])
             ticker++
         }
     }
@@ -39,7 +75,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         if (ticker == 2) {
-            Log.d(POEM, list_poem.get(ticker))
+            Log.d(POEM, list_poem[ticker])
             ticker++
         }
     }
@@ -47,7 +83,7 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         if (ticker == 3) {
-            Log.d(POEM, list_poem.get(ticker))
+            Log.d(POEM, list_poem[ticker])
             ticker++
         }
     }
@@ -55,7 +91,7 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         if (ticker == 4) {
-            Log.d(POEM, list_poem.get(ticker))
+            Log.d(POEM, list_poem[ticker])
             ticker++
         }
     }
@@ -63,7 +99,7 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         if (ticker == 5) {
-            Log.d(POEM, list_poem.get(ticker))
+            Log.d(POEM, list_poem[ticker])
             ticker++
         }
     }
---
+++ app/src/main/java/prot/zr/a5/Reader.kt
@@ -0,0 +1,11 @@
+package prot.zr.a5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reader : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reader)
+    }
+}
